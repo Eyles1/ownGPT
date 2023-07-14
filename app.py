cf6e2ec86fa1ad09b62c5538458238a249852681
Eyles1 <54001277+Eyles1@users.noreply.github.com>
2023-07-14 14:25:28 +0200

    Basic Answer Streaming (BETA)

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, send_file, request, Response
+from ownGPT import ownGPT
+import sys
 import os
 
 
@@ -29,5 +31,25 @@ def add_assets(directory):
 
 add_assets(os.path.join(os.getcwd(), "templates/"))
 
+
+class LazyModel:
+    def __init__(self, model_name: str):
+        self.model_name = model_name
+        self._model = None
+    
+    @property
+    def model(self):
+        if self._model is None:
+            self._model = ownGPT(self.model_name)
+        return self._model
+
+@app.route("/answer", methods = ["POST"])
+def answer():
+    return Response(model.model.generate(**request.json), mimetype="text/txt")
+
 if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        model = LazyModel(" ".join(sys.argv[1:]))
+    else:
+        model = LazyModel("orca-mini-3b.ggmlv3.q4_0.bin")
     app.run()
