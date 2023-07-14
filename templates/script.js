75af15d5237300d9d7fdec6470c3f53edfce6ea9
Eyles1 <54001277+Eyles1@users.noreply.github.com>
2023-07-14 15:12:36 +0200

    Client Chatting (BETA)

--- templates/script.js
+++ templates/script.js
@@ -1,38 +1,52 @@
 function sendMessage() {
-    var userInput = document.getElementById("user-input");
-    var message = userInput.value;
-    userInput.value = "";
+  var userInput = document.getElementById("user-input");
+  var message = userInput.value;
+  userInput.value = "";
   
-    displayUserMessage(message);
-    getBotResponse(message);
-  }
+  displayUserMessage(message);
+  getBotResponse(message);
+}
   
-  function displayUserMessage(message) {
-    var chatMessages = document.getElementById("chat-messages");
-    var userMessageElement = document.createElement("div");
-    userMessageElement.classList.add("user-message");
-    var span = document.createElement("span");
-    span.innerText = message;
-    userMessageElement.appendChild(span);
-    chatMessages.appendChild(userMessageElement);
-  }
+function displayUserMessage(message) {
+  var chatMessages = document.getElementById("chat-messages");
+  var userMessageElement = document.createElement("div");
+  userMessageElement.classList.add("user-message");
+  var span = document.createElement("span");
+  span.innerText = message;
+  userMessageElement.appendChild(span);
+  chatMessages.appendChild(userMessageElement);
+}
   
-  function displayBotMessage(message) {
-    var chatMessages = document.getElementById("chat-messages");
-    var botMessageElement = document.createElement("div");
-    botMessageElement.classList.add("bot-message");
-    var span = document.createElement("span");
-    span.innerText = message;
-    botMessageElement.appendChild(span);
-    chatMessages.appendChild(botMessageElement);
+function displayBotMessage(message) {
+  var chatMessages = document.getElementById("chat-messages");
+  var botMessageElement = document.createElement("div");
+  botMessageElement.classList.add("bot-message");
+  var span = document.createElement("span");
+  span.innerText = message;
+  var current_date = new Date();
+  span.id = String(current_date.getTime());
+  botMessageElement.appendChild(span);
+  chatMessages.appendChild(botMessageElement);
+  return span.id;
+}
+
+function getBotResponse(userMessage) {
+  // Bot Communication
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "/answer");
+  xhr.setRequestHeader("Content-Type", "application/json");
+  let output_id = displayBotMessage("Thinking ...");
+  xhr.onreadystatechange = function (){
+    document.getElementById(output_id).innerText = this.response;
   }
-  
-  function getBotResponse(userMessage) {
-    // Bot Communication
-    var botResponses = ["Hallo!", "Wie kann ich dir helfen?", "Das ist interessant!"];
-    var randomIndex = Math.floor(Math.random() * botResponses.length);
-    var botResponse = botResponses[randomIndex];
-    
-    displayBotMessage(botResponse);
+  conversation = [];
+  for (const msg of document.getElementById("chat-messages").children){
+    conversation.push({
+      "role": (msg.className == "user-message" ? "user" : "assistant"),
+      "content": msg.innerText
+    });
   }
+  conversation.pop();
+  xhr.send(JSON.stringify({"prompt": userMessage, "conversation": conversation}));
+}
   
